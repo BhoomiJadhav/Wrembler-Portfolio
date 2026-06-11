eb2bd7e8eb9b53ef57be105ea8146d82d8491c68
Bhoomi Jadhav <jadhavbhoomi16@gmail.com>
2026-06-11 22:34:58 +0530

    modakalay added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import HeroSection from "./sections/HeroSection";
-import MarqueeSection from "./sections/MarqueeSection";
-import AboutSection from "./sections/AboutSection";
-import ServicesSection from "./sections/ServicesSection";
-import ProjectsSection from "./sections/ProjectsSection";
-import ContactSection from "./sections/ContactSection";
-import Footer from "./sections/Footer";
+// import HeroSection from "./sections/HeroSection";
+// import MarqueeSection from "./sections/MarqueeSection";
+// import AboutSection from "./sections/AboutSection";
+// import ServicesSection from "./sections/ServicesSection";
+// import ProjectsSection from "./sections/ProjectsSection";
+// import ContactSection from "./sections/ContactSection";
+// import Footer from "./sections/Footer";
 
+// export default function App() {
+//   return (
+//     <main className="overflow-x-clip bg-[#0C0C0C] font-kanit">
+//       <HeroSection />
+//       <MarqueeSection />
+//       <AboutSection />
+//       <ServicesSection />
+//       <ProjectsSection />
+//       <ContactSection />
+//       <Footer />
+//     </main>
+//   );
+// }
 export default function App() {
   return (
-    <main className="overflow-x-clip bg-[#0C0C0C] font-kanit">
-      <HeroSection />
-      <MarqueeSection />
-      <AboutSection />
-      <ServicesSection />
-      <ProjectsSection />
-      <ContactSection />
-      <Footer />
+    <main className="bg-black text-white min-h-screen">
+      <h1>Hello Wrembler</h1>
     </main>
   );
 }
